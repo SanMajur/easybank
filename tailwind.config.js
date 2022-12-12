51dd9cc5d1493ef2bbc2403b6b1268608ce56b36
santino majur <demalong22@gmail.com>
2022-12-12 18:18:51 +0300

    responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pubSans : "Public Sans"
+      },
+      colors: {
+        primary: {
+         'Dark-Blue': 'hsl(233, 26%, 24%)',
+         'Lime-Green': 'hsl(136, 65%, 51%)',
+         'Bright-Cyan': 'hsl(192, 70%, 51%)',
+        },
+        neutral: {
+          'Grayish-Blue': 'hsl(233, 8%, 62%)',
+          'Light-Grayish-Blue': 'hsl(220, 16%, 96%)',
+          'Very-Light-Gray': 'hsl(0, 0%, 98%)',
+          'White': 'hsl(0, 0%, 100%)',
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.5rem',
+          sm: '3rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
+    },
   },
   plugins: [],
 }
