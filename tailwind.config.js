c23d87b4332666f265050a80112f28c843f0b4f9
santino majur <demalong22@gmail.com>
2023-03-06 23:40:08 +0300

    responsive hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     container: {
       center: true,
     },
+    minHeight: {
+      '20rem': '20rem',
+      '41rem': '41rem',
+    },
     extend: {
       fontFamily: {
         'Alegreya': ['Alegreya', 'serif']
@@ -12,10 +16,18 @@ module.exports = {
       colors: {
         'lightGray': 'hsl(233deg, 8%, 62%)',
         'veryLightGray': 'hsl(0deg, 0%, 100%)',
+        'veryLightGrayish': 'hsl(0deg, 0%, 98%)'
       },
       transformOrigin: {
         'topRightLeft': '0.1875rem 0.0625rem'
       },
+      backgroundImage: {
+        'mobile-bg': "url('/src/images/bg-intro-mobile.svg')",
+        'desktop-bg': "url('/src/images/bg-intro-desktop.svg')"
+      },
+      spacing: {
+        '-32.5': '-8.5rem'
+      }
     },
   },
   plugins: [],
